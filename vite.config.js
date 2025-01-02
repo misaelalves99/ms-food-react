7254b4d21a7b2f0bf64a6331ca70e830fe33d016
Misael Alves <112561288+misaelalves99@users.noreply.github.com>
2025-01-02 02:11:14 -0300

    Delete api

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // substitua pela URL da sua API
-    },
-  },
 })
